13b913bb04cd40699838055f4fc34b549f10583a
necati <necatisozer@gmail.com>
2021-03-23 22:28:01 +0300

    Constraint Layout

---
+++ app/src/main/java/com/necatisozer/dsc/myapplication/BoxActivity.kt
@@ -0,0 +1,11 @@
+package com.necatisozer.dsc.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BoxActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_box)
+    }
+}
